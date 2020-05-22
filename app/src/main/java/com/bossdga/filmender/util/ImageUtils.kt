5702e475b5ec8b262edf4243e9cc311416cd30a8
bossdga <diego.gomez.alonso>
2020-05-22 09:15:07 +0200

    switched from picasso to coil

--- app/src/main/java/com/bossdga/filmender/util/ImageUtils.kt
+++ app/src/main/java/com/bossdga/filmender/util/ImageUtils.kt
@@ -1,13 +1,13 @@
 package com.bossdga.filmender.util
 
 import android.widget.ImageView
-import com.squareup.picasso.Picasso
+import coil.api.load
 
 /**
  * Utility class to deal with images
  */
 object ImageUtils {
-    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/"
+    private const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/"
     /**
      * Sets an image to an ImageView from an url
      * @param context
@@ -15,6 +15,6 @@ object ImageUtils {
      * @param url
      */
     fun setImage(image: ImageView, url: String?) {
-        Picasso.get().load(IMAGE_BASE_URL + url).into(image)
+        image.load(IMAGE_BASE_URL + url)
     }
 }
