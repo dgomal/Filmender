4ed65a21a6fd0f06973c83911bc5398a77791b45
bossdga <diego.gomez.alonso>
2020-05-31 16:36:37 +0200

    added icons for watchlist

--- app/src/main/java/com/bossdga/filmender/presentation/ui/activity/FilterSettingsActivity.kt
+++ app/src/main/java/com/bossdga/filmender/presentation/ui/activity/FilterSettingsActivity.kt
@@ -1,10 +1,13 @@
 package com.bossdga.filmender.presentation.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import com.bossdga.filmender.R
 import com.bossdga.filmender.presentation.viewmodel.BaseViewModel
 import com.bossdga.filmender.presentation.viewmodel.MainViewModel
+
 
 class FilterSettingsActivity : BaseActivity<BaseViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,19 @@ class FilterSettingsActivity : BaseActivity<BaseViewModel>() {
 
     override fun createViewModel(): MainViewModel {
         return ViewModelProvider(this, factory).get(MainViewModel::class.java)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                startActivity(Intent(this, MainActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
