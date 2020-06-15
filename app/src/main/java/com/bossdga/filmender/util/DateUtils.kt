218b882bdb83d4756e17bdfee5821303a139721e
bossdga <diego.gomez.alonso>
2020-06-15 08:57:04 +0200

    solved a crash with runtime being null

--- app/src/main/java/com/bossdga/filmender/util/DateUtils.kt
+++ app/src/main/java/com/bossdga/filmender/util/DateUtils.kt
@@ -7,8 +7,8 @@ import java.util.concurrent.TimeUnit
  */
 object DateUtils {
     @JvmStatic
-    fun fromMinutesToHHmm(minutes: Int?): String {
-        val hours = TimeUnit.MINUTES.toHours(minutes?.toLong()!!)
+    fun fromMinutesToHHmm(minutes: Int): String {
+        val hours = TimeUnit.MINUTES.toHours(minutes.toLong())
         val remainMinutes = minutes.minus(TimeUnit.HOURS.toMinutes(hours))
         return String.format("%02d:%02d", hours, remainMinutes)
     }
