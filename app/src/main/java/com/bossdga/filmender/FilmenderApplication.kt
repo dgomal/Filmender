54251ca9e335f1511b80efd80b0178325e633b6b
bossdga <diego.gomez.alonso>
2020-06-18 10:44:55 +0200

    new util class for unique id

--- app/src/main/java/com/bossdga/filmender/FilmenderApplication.kt
+++ app/src/main/java/com/bossdga/filmender/FilmenderApplication.kt
@@ -4,12 +4,15 @@ import android.app.Application
 import android.content.Context
 import androidx.preference.PreferenceManager
 import com.bossdga.filmender.source.network.RetrofitService
+import com.bossdga.filmender.util.InstallationUtils
 import com.bossdga.filmender.util.PreferenceUtils
 import com.google.android.gms.ads.MobileAds
 
 class FilmenderApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        InstallationUtils.id(this)
 
         RetrofitService.initWith(this)
         PreferenceUtils.initWith(this)
